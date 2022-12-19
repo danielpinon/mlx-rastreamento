34950e84558830029a52e118a1b80fb081d70bd7
danielpinon <danielpinon@danielpinon.com.br>
2022-12-19 04:42:47 -0300

    Feat: Ajustes do layout do sistema

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
                 <i class="material-icons">notifications_active</i>
               </div>
               <p class="card-category">Nº de Alertas</p>
-              <h3 class="card-title">3</h3>
+              <h3 class="card-title">0</h3>
             </div>
             <div class="card-footer">
               <div class="stats">

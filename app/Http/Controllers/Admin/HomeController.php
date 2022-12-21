9cc67ac07ef7d87a986f6fb2c510ff869ec91d5d
danielpinon <danielpinon@danielpinon.com.br>
2022-12-21 10:14:22 -0300

    Feat: Melhorias do sistema

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Repositories\FaccoesRepository;
+use App\Repositories\LotesRastreamentoRepository;
 
 class HomeController extends Controller
 {
@@ -12,9 +14,14 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(
+        FaccoesRepository $faccoesRepository,
+        LotesRastreamentoRepository $lotesRastreamentoRepository
+    )
     {
         $this->middleware('auth');
+        $this->faccoesRepository = $faccoesRepository;
+        $this->lotesRastreamentoRepository = $lotesRastreamentoRepository;
     }
 
     /**
@@ -24,6 +31,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('dashboard');
+        $faccoes = $this->faccoesRepository->findWhere(['FAC_STATUS' => true]);
+        $lotes = $this->lotesRastreamentoRepository->all();
+        return view('dashboard', compact('faccoes','lotes'));
     }
 }

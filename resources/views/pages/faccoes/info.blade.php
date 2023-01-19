82a6bdfb17c81555acebc70fda77c49396dff6dd
danielpinon <danielpinon@danielpinon.com.br>
2023-01-19 16:00:16 -0300

    Fix: Correção de bug do sistema

--- resources/views/pages/faccoes/info.blade.php
+++ resources/views/pages/faccoes/info.blade.php
@@ -51,7 +51,7 @@
                                 <i class="material-icons">info</i>
                             </div>
                             <p class="card-category">Lotes de Trabalho Total</p>
-                            <h3 class="card-title">0</h3>
+                            <h3 class="card-title">{{ $faccao->lotes->count() }}</h3>
                         </div>
                         <div class="card-footer">
                             {{ date('d/m/Y H:i:s') }}
